75132d38ea7fc4dc8cb51a08c2655bec4359955e
Gilberto <gilbertoej99@gmail.com>
2021-06-19 11:29:39 -0300

    adc-components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatoPageComponent } from './components/contato-page/contato-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { InstituicoesPageComponent } from './components/instituicoes-page/instituicoes-page.component';
+import { LeisPageComponent } from './components/leis-page/leis-page.component';
+import { MenuTemplateComponent } from './components/menu-template/menu-template.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'',
+  component: MenuTemplateComponent,
+  children: [
+    {
+    path: '',
+    component: HomePageComponent
+    },
+    {
+      path: 'leis',
+      component: LeisPageComponent
+    },
+    {
+      path: 'instituicoes',
+      component: InstituicoesPageComponent
+    },
+    {
+      path: 'contato',
+      component : ContatoPageComponent
+    }
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
